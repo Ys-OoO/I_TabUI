52dfb8052efeccac5df96fba6590b7b44ada6387
haunghe <1959974025@qq.com>
2023-10-17 17:07:29 +0800

    优化markDown

--- src/pages/Article/AddArticle/index.jsx
+++ src/pages/Article/AddArticle/index.jsx
@@ -1,48 +1,108 @@
-import MdEditor from 'for-editor';
-import React, { useState } from "react";
-import styles from '../styles.less'
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+import React, { useEffect, useState } from 'react';
+import styles from '../styles.less';
+import style from './style.less'
+import {Button, Form, Input,Card,Tooltip} from 'antd';
+import {
+  SafetyCertificateFilled
+} from '@ant-design/icons';
+// 导入编辑器的样式，不导入会出现毫无样式情况
+import 'react-markdown-editor-lite/lib/index.css';
+import { useLocation} from '@umijs/max';
 const AddArticle = ()=>{
-   // 工具栏菜单
-   const toolbar = {
-    h1: true, // h1
-    h2: true, // h2
-    h3: true, // h3
-    h4: true, // h4
-    img: true, // 图片
-    link: true, // 链接
-    code: true, // 代码块
-    preview: true, // 预览
-    expand: true, // 全屏
-    /* v0.0.9 */
-    undo: true, // 撤销
-    redo: true, // 重做
-    save: true, // 保存
-    /* v0.2.3 */
-    subfield: true, // 单双栏模式
-  };
-  // 保存Markdown文本内容
-  const [mdContent, setMdContent] = useState('')
+      const location = useLocation();
+      const parts = location.pathname.split('=');
+      let  articleId =parts[parts.length - 1];
+      const [form] = Form.useForm();
+      const [formData,setFormData] =useState(null);
 
-  // 上传图片
-  function uploadImg (file) {
-    console.log('file', file);
-  };
-  // 输入内容改变
-  function handleEditorChange (value) {
-    console.log('handleChange', value);
-    setMdContent(value)
-  }
-  // 保存输入内容
-  function handleEditorSave (value) {
-    console.log('handleEditorSave', value);
-  }
-   return (
-      <div className={styles.mainBox}>
-          <MdEditor placeholder="请输入文章内容" height={600} lineNum={false}
-          toolbar={toolbar} value={mdContent} onChange={handleEditorChange} onSave={handleEditorSave} addImg={uploadImg} />
-      </div>
-     
-
-   );
+      // 数据保存
+      const [mdContent, setMdContent] = useState("");
+      // markdown-it 利用设置参数，具体查询markdown-it官网
+      const mdParser = new MarkdownIt({
+        html: true,
+        linkify: false,
+        typographer: true
+          }).enable('image');
+      // 检测markdown数据变化
+      function handleEditorChange({html, text}) {
+        setMdContent(text)
+        console.log('handleEditorChange', html, text);
+      };
+      const onFinish = () => {
+        const formList = form.getFieldsValue();
+        const list ={...formList, mdContent};
+        //发请求（分为编辑文章接口和新增文章接口）
+      };
+      return (
+          <div className={styles.mainBox}>
+             <Card>
+             <div className={style.MdHeaderBox}>
+                <span style={{fontSize:'25px',color:'black'}}>{articleId!=='null'?'编辑':'新建'}文章</span>
+               
+                <Tooltip title="保存文章">
+                <Button icon={<SafetyCertificateFilled style={{ fontSize: '24px' }}/> } shape='circle' type='primary' size='large' onClick={onFinish}></Button>
+                </Tooltip>
+              </div>
+               <Form
+                  style={{
+                    maxWidth: 600,
+                  }}
+                  onFinish={onFinish}
+                  autoComplete="off"
+                  disabled={articleId!=='null'?true:false}
+                  form={form}
+                >
+                  <Form.Item
+                    label="标题"
+                    name="articleTitle"
+                    rules={[
+                      {
+                        required: true,
+                        message: '请输入文章标题',
+                      },
+                    ]}
+                   
+                  >
+                    <Input/>
+                  </Form.Item>
+              
+                  <Form.Item
+                    label="&nbsp;&nbsp;描述"
+                    name="articleDescription"
+                  >
+                    <Input.TextArea allowClear autoSize={{minRows:2}}/>
+                  </Form.Item>
+                 </Form>
+                 <MdEditor
+                    value={mdContent}
+                    onChange={handleEditorChange}
+                    renderHTML={text => mdParser.render(text)}
+                    style={
+                      {height: 400}
+                    }
+                    onImageUpload={
+                      async (file) => {
+                        const formData = new FormData()
+                        formData.append('files', file)
+                        // 自行图片上传功能，利用form文件表单
+                        return await postUrl(formData).then(
+                          (res => {
+                            if (res.code === 200) {
+                              return res.data
+                            } else {
+                              message.error("图片过大，请上传小于1mb的图片")
+                            }
+                          })
+                        )
+                      }
+                    }
+                   >
+                </MdEditor>
+             </Card>
+            
+          </div>
+        )
 }
 export default AddArticle;
