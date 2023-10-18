9038e030319b594365a78748308f44b84916fc49
ys <yangsenwb303@hellobike.com>
2023-10-18 17:16:39 +0800

    format

--- src/pages/Article/ArticleList/MyCard.jsx
+++ src/pages/Article/ArticleList/MyCard.jsx
@@ -1,13 +1,10 @@
-import style from './style.less';
-import React, { useState } from 'react';
+import { DeleteFilled, EditFilled } from '@ant-design/icons';
 import { Link } from '@umijs/max';
-import { Button, Space,Image} from 'antd';
-import {
-  DeleteFilled,
-  EditFilled
-} from '@ant-design/icons';
-const MyCard = ({article})=>{
-   const {articleId} = article;
+import { Button, Image, Space } from 'antd';
+import { useState } from 'react';
+import style from './style.less';
+const MyCard = ({ article }) => {
+  const { articleId } = article;
   const [isHovered, setIsHovered] = useState(false);
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -17,33 +14,38 @@ const MyCard = ({article})=>{
     setIsHovered(false);
   };
 
-    return (
-
-              <div className={style.card} 
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}
-                style={{margin:'0px',marginRight:'0px'}}
-              >
-                <div className={style.cardTop}></div>
-                <div className={style.imgContainer}>
-                  <Image
-                     src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-                     width={70}
-                     style={{ borderRadius: '15px'}}
-                />
-                </div>
-                <span className={style.articleTitle}>{article.articleTitle}</span>
-                <p className={style.describe}>{article.articleDescription}</p>
-                {isHovered && (
-                    <Space className={style.buttonContainer} size='middle'>
-                    <Link to={`/addArticle/articleId=${articleId}`}>  
-                      <Button shape="circle" icon={<EditFilled />}/>
-                    </Link>
-                    <Button shape="circle" icon={<DeleteFilled />}  danger type="primary" onClick={() => alert('删除按钮被点击')}>
-                    </Button>
-                  </Space>
-                )}
-              </div>
-    )
+  return (
+    <div
+      className={style.card}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ margin: '0px', marginRight: '0px' }}
+    >
+      <div className={style.cardTop}></div>
+      <div className={style.imgContainer}>
+        <Image
+          src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          width={70}
+          style={{ borderRadius: '15px' }}
+        />
+      </div>
+      <span className={style.articleTitle}>{article.articleTitle}</span>
+      <p className={style.describe}>{article.articleDescription}</p>
+      {isHovered && (
+        <Space className={style.buttonContainer} size="middle">
+          <Link to={`/addArticle/articleId=${articleId}`}>
+            <Button shape="circle" icon={<EditFilled />} />
+          </Link>
+          <Button
+            shape="circle"
+            icon={<DeleteFilled />}
+            danger
+            type="primary"
+            onClick={() => alert('删除按钮被点击')}
+          ></Button>
+        </Space>
+      )}
+    </div>
+  );
 };
 export default MyCard;
--- src/pages/Home/SearchInput/SearchInput.js
+++ src/pages/Home/SearchInput/SearchInput.js
@@ -1,5 +1,5 @@
 import { BaiduIcon, GithubIcon } from '@/components/icons'
-import { Select, Space, Input,List } from 'antd'
+import { Select, Space, Input, List } from 'antd'
 import React, { useEffect, useState } from 'react'
 import _ from 'lodash';
 import { isRelNull } from '@/utils/common';
@@ -26,7 +26,7 @@ export default function SearchInput({ otherSearchConfig = [], inputProps, select
   ]
   const [currentSearchSite, setCurrentSearchSite] = useState('baidu');
   const [searchValue, setSearchValue] = useState(null);
-  const [data,setData] = useState(null);
+  const [data, setData] = useState(null);
   const onSearch = (value, e, { source }) => {
     if (isRelNull(value) || source === 'clear') {
       return;
@@ -39,69 +39,73 @@ export default function SearchInput({ otherSearchConfig = [], inputProps, select
       }
     })
   }
-   const fetchData = ()=>{
-     if(searchValue&&currentSearchSite==='baidu') {
+
+  //通过jsonp获取百度提供的搜索下拉词
+  const fetchData = () => {
+    if (searchValue && currentSearchSite === 'baidu') {
       jsonp(`https://www.baidu.com/sugrec?&prod=pc&wd=${searchValue}`)
-       .then((res)=>res.json())
-        .then((d)=>{
-          if(d){
-            const {g} =d;
-            if(g){
-              let dataList = g.map((item)=>{
+        .then((res) => res.json())
+        .then((d) => {
+          if (d) {
+            const { g } = d;
+            if (g) {
+              let dataList = g.map((item) => {
                 return {
-                  label:item.q,
-                  value:item.sa,
+                  label: item.q,
+                  value: item.sa,
                 }
-             });
-             setData(dataList);
+              });
+              setData(dataList);
             }
           }
-        }) 
-     }else{
-       setData(null);
-     }
-       
-   };
-   const getSearchValue=(e)=>{
-      const content =e.target.innerText;
-      setSearchValue(content);
-   }
-   useEffect(()=>{
-      fetchData();
-   },[searchValue]);
+        })
+    } else {
+      setData(null);
+    }
+
+  };
+
+  const getSearchValue = (e) => {
+    const content = e.target.innerText;
+    setSearchValue(content);
+  }
+  useEffect(() => {
+    fetchData();
+  }, [searchValue]);
+
   return (
     <div className={style.container}>
-        <Space.Compact size='large' {...props}>
-      <Select
-        defaultValue="baidu"
-        options={searchConfig}
-        value={currentSearchSite}
-        onChange={(value) => { setCurrentSearchSite(value) }}
-        {...selectProps}
-      />
-      <Input.Search
-        placeholder='输入搜索内容'
-        allowClear
-        onSearch={onSearch}
-        value={searchValue}
-        onChange={(e) => { setSearchValue(e.target.value) }}
-        {...inputProps} />
-    </Space.Compact>
-       <div className={style.listContent}>
-       {data&&<List
-            bordered
-            dataSource={data}
-            renderItem={(item) => (
+      <Space.Compact size='large' {...props}>
+        <Select
+          defaultValue="baidu"
+          options={searchConfig}
+          value={currentSearchSite}
+          onChange={(value) => { setCurrentSearchSite(value) }}
+          {...selectProps}
+        />
+        <Input.Search
+          placeholder='输入搜索内容'
+          allowClear
+          onSearch={onSearch}
+          value={searchValue}
+          onChange={(e) => { setSearchValue(e.target.value) }}
+          {...inputProps} />
+      </Space.Compact>
+      <div className={style.listContent}>
+        {data && <List
+          bordered
+          dataSource={data}
+          renderItem={(item) => (
             <List.Item>
-                <a onClick={getSearchValue} style={{color:'#ffff'}}>
-                 {item.label}
-                </a>
+              <a onClick={getSearchValue} style={{ color: '#ffff' }}>
+                {item.label}
+              </a>
             </List.Item>
           )}
         />}
-       </div>
+      </div>
     </div>
-  
+
 
   )
 }
