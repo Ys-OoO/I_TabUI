2ae162f0d27d19be559ff2338e4629f342de1b0f
haunghe <1959974025@qq.com>
2023-10-24 14:41:42 +0800

    feat: 添加任务编辑功能

--- src/components/styleBox.js
+++ src/components/styleBox.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 export const Flex = styled.div`
   display : flex;
-  align-items:center;
 `;
 
 export const FlexColumn = styled(Flex)`
--- src/pages/Todo/TodoItem.jsx
+++ src/pages/Todo/TodoItem.jsx
@@ -7,8 +7,10 @@ import {
 import { FlexAuto, FlexColumn, FlexRowAuto } from '@/components/styleBox';
 import { DeleteTwoTone } from '@ant-design/icons';
 import { useDispatch, useSelector } from '@umijs/max';
+import { Input, message } from 'antd';
 import dayjs from 'dayjs';
 import _ from 'lodash';
+import { useState } from 'react';
 import style from './style.less';
 
 const getIcon = (status) => {
@@ -26,8 +28,10 @@ const getIcon = (status) => {
 
 export default function TodoItem({ todo, innerRef, ...props }) {
   const dispatch = useDispatch();
-  const { content = '-', time = dayjs().format('MM月DD日 HH:mm') } = todo;
+  const { content = '-', time = dayjs().format('MM月DD日 HH:mm'), id } = todo;
   const { todoList } = useSelector((state) => state.todo);
+  const [editor, setEditor] = useState(false);
+  const [inputContent, setInputContent] = useState(content);
 
   const deleteTodo = () => {
     const newTodoList = _.filter(todoList, (t) => {
@@ -41,11 +45,62 @@ export default function TodoItem({ todo, innerRef, ...props }) {
       config: { todoList: newTodoList },
     });
   };
+  const handleEditor = () => {
+    setEditor(true);
+  };
+  const handleChange = (e) => {
+    setInputContent(e.target.value);
+  };
+  const finishEditor = () => {
+    if (inputContent) {
+      setEditor(false);
+      let newTodoList = JSON.parse(JSON.stringify(todoList));
+      let time = new Date();
+      let newTodoList_copy = newTodoList.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            content: inputContent,
+            time,
+          };
+        } else
+          return {
+            ...todo,
+          };
+      });
+      dispatch({
+        type: 'todo/saveLocalTodoList',
+        config: { todoList: newTodoList_copy },
+      });
+    } else {
+      message.error('任务不能为空！');
+    }
+  };
 
   return (
     <FlexRowAuto ref={innerRef} {...props} className={style.todoItem}>
       <FlexColumn style={{ width: '70%' }} className={style.todoLeft}>
-        <div className={style.content}>{content}</div>
+        <br />
+        <div
+          className={style.content}
+          onMouseDown={handleEditor}
+          style={{ cursor: 'pointer' }}
+        >
+          {editor === true && todo.status !== 'done' ? (
+            <Input
+              autoFocus
+              value={inputContent}
+              onPressEnter={finishEditor}
+              onChange={handleChange}
+              bordered={false}
+              className={style.editorInput}
+              allowClear
+              placeholder="回车完成修改"
+            />
+          ) : (
+            content
+          )}
+        </div>
         <FlexAuto />
         <div className={style.time}>{dayjs(time).format('MM月DD日 HH:mm')}</div>
       </FlexColumn>
