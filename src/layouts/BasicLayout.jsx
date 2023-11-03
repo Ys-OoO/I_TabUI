39f0fa1c60b24ad1a687d0ed9fe23659f0fc909d
ys <241280843@qq.com>
2023-11-04 01:04:46 +0800

    废弃indexedDb工具类，选用Dexie替代

--- src/layouts/BasicLayout.jsx
+++ src/layouts/BasicLayout.jsx
@@ -3,13 +3,18 @@ import { Flex, FlexColumn } from '@/components/styleBox';
 import { Outlet, useDispatch, useSelector } from '@umijs/max';
 import { useEffect } from 'react';
 import BasicNavigator from './BasicNavigator';
+import Dexie from 'dexie';
 
 export default function BasicLayout() {
   const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
 
   useEffect(() => {
-
+    const db = new Dexie('itab');
+    db.version(1).stores({
+      favoritesFolder:'++id, typeName',
+      favoritesItem:'++id, forderId, url, name , cover'
+    });
   }, []);
 
   return (
